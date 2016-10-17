f09d4bd7eca016efd31b6d8739fa0f7139d7ab60
Yman <Yman>
2016-10-17 11:31:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yman/sensor/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.yman.sensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private TextView show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        show = (TextView) findViewById(R.id.show);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(sensorManager != null){
+            sensorManager.unregisterListener(listener);
+        }
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float xValue = Math.abs(sensorEvent.values[0]);
+            float yValue = Math.abs(sensorEvent.values[1]);
+            float zValue = Math.abs(sensorEvent.values[2]);
+            if(xValue > 15 || yValue > 15 || zValue > 15){
+                show.setText("用户摇动了手机");
+            }
+            else{
+                show.setText("摇动未触发");
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+}
